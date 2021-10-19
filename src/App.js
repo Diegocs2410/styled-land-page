0ec9c8eb750b3d5c2010c5e0dfc9d4629dfa7813
Diego Armando Carreño Serrano <89321032+Diegocs2410@users.noreply.github.com>
2021-10-19 17:05:43 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 classaName='text-center mt-5 fw-bold text-primary'>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
